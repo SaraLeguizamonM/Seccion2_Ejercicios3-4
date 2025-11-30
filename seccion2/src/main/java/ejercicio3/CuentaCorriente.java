684165d087b524e9de4bc78e78eff2a7e63ab3a6
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-30 06:44:02 -0500

    Agregar logica en cada metodo en CuentaCorriente
    En depositar se valida si es positivo el monto
    En retirar se tiene un limite de sobre giro y valida si esta dentro del rango para retirar dinero

--- seccion2/src/main/java/ejercicio3/CuentaCorriente.java
+++ seccion2/src/main/java/ejercicio3/CuentaCorriente.java
@@ -1,20 +1,27 @@
 package ejercicio3;
 
 public class CuentaCorriente extends Cuenta{
+    private final double liimiteDeSobreGiro = -50000; // Limite de sobregiro de -50000
 
-    public CuentaCorriente(double saldo, String cuenta) {
+    public CuentaCorriente(double saldo, String cuenta) { // Constructor
         this.saldo = saldo;
         this.cuenta = cuenta;
 
     }
 
     @Override
-    void depositar(double monto) {
-        saldo = monto + saldo;
+    void depositar(double monto) { // Depositar dinero
+        if (monto > 0) { // Validar monto positivo
+            saldo += monto;
+        }
     }
 
     @Override
-    void retirar(double monto) {
-
+    Boolean retirar(double monto) { // Retirar dinero si no excede el limite de sobregiro
+        if (monto > 0 && saldo - monto >= liimiteDeSobreGiro) { // Validar monto positivo y limite de sobregiro
+            saldo -= monto; // Restar monto del saldo
+            return true; // Retiro en caso de que sea exitoso
+        }
+        return false; // Retiro fallido
     }
 }
