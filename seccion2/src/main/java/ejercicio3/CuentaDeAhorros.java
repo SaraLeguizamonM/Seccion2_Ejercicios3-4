c53bbf6a04fb7c313b943008eda51ea185afd0e7
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-30 06:40:28 -0500

    Agregar logica en cada metodo en la clase CuentaDeAhorros
    En depositar se hace la validacion de que sea positivo y agregar el interes
    En retirar se verifica si alcanza el dinero y resta en caso de que haya suficiente dinero

--- seccion2/src/main/java/ejercicio3/CuentaDeAhorros.java
+++ seccion2/src/main/java/ejercicio3/CuentaDeAhorros.java
@@ -1,17 +1,27 @@
 package ejercicio3;
 public class CuentaDeAhorros extends Cuenta {
+    private final double interesDiario = 0.10 / 365; // Interes anual del 10% dividido por 365 días
 
-    public CuentaDeAhorros(double saldo, String cuenta) {
+    public CuentaDeAhorros(double saldo, String cuenta) { // Constructor
         this.saldo = saldo;
         this.cuenta = cuenta;
     }
 
     @Override
-    void depositar(double monto) {
-         saldo = monto + saldo;
+    void depositar(double monto) { // Depositar dinero y aplicar interes diario
+        if (monto > 0) { // Validar monto positivo
+            saldo += monto; // Sumar monto al saldo
+            double interes = saldo * interesDiario; // Calcular interes diario
+            saldo += interes; // Agregar interes al saldo
+        }
     }
 
     @Override
-    void retirar(double monto) {
+    Boolean retirar(double monto) { // Retirar dinero si hay suficiente saldo
+        if (monto > 0 && monto <= saldo) { // Validar monto positivo y suficiente saldo
+            saldo -= monto; // Restar monto del saldo
+            return true; // Retiro en caso que sea exitoso
+        }
+        return false; // Retiro fallido
     }
 }
