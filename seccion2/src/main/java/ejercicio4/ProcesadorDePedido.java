a1ae0c3ae006b719291398552e61d921de193888
Avr1lls <saraleguizamon1234@gmail.com>
2025-11-30 21:58:33 -0500

    Agregar clase procesadorDePedido con sus respectivos metodos: abstract y concreto

---
+++ seccion2/src/main/java/ejercicio4/ProcesadorDePedido.java
@@ -0,0 +1,9 @@
+package ejercicio4;
+
+public abstract class ProcesadorDePedido {
+
+    public abstract void procesar ();
+    public void validarPedido() {
+        System.out.println("Validando el pedido...");
+    }
+}   
